e935450e8373030d0be44caa57ec75c13e065ee6
Umar-Mahmood <Umarmahmood637@gmail.com>
2024-10-02 04:14:10 +0900

    added all orgins in cors 5

--- extensive_api/app.js
+++ extensive_api/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const storyRoutes = require("./routes/storyRoutes");
 
